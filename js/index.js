024f27890a7745f545f1c94852e4bbfadd3b2031
darkchen <70423199@qq.com>
2016-05-11 15:41:51 +0800

    first changes and prepare
    changed some base file and add index page , vender file

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(function() {
+    var slider;
+
+    function initSize() {
+        $('.slider div').css({
+            'height': $(window).height() + 'px',
+            'width': $(window).width() + 'px'
+        })
+    }
+    initSize();
+    slider = $('.slider').unslider({
+        autoplay: true,
+        speed: 2000,
+        delay: 5000,
+        animation: 'fade',
+        nav: false,
+        arrows: false
+    });
+    $(window).resize(function() {
+        initSize();
+    })
+});
