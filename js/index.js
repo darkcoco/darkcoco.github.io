a0a5f9aad6d360e502d15a0ba0ca2995ef2ebed3
darkchen <70423199@qq.com>
2016-05-28 11:46:31 +0800

    add first artical
    优化分类导航，增加第一篇文章

--- js/index.js
+++ js/index.js
@@ -10,8 +10,8 @@ $(function() {
     initSize();
     slider = $('.slider').unslider({
         autoplay: true,
-        speed: 2000,
-        delay: 5000,
+        speed: 5000,
+        delay: 8000,
         animation: 'fade',
         nav: false,
         arrows: false
@@ -20,4 +20,7 @@ $(function() {
         initSize();
     });
 
+    $.getJSON('http://open.iciba.com/dsapi/?callback=?', function(msg) {
+        $('.main-content').parents('section').before('<section class="container animated fadeIn"><div class="row"><div class="alert alert-info" role="alert">' + msg.content +'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+ msg.note + '</div></div></section>');
+    });
 });
